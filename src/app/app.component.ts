2363f92c7e71d583fc97d198abae2e235bf36d35
huyfr <61008601+huyfr@users.noreply.github.com>
2020-07-02 10:43:49 +0700

    Thực hành hiển thị thông tin thú nuôi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'thThayDoiFontSize';
+  title = 'Bài tập về tổng quan angular';
 }
