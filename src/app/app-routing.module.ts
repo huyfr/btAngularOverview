1d5380b85404c6a73e1d8a61d8ae352ea64fcaeb
huyfr <61008601+huyfr@users.noreply.github.com>
2020-07-02 20:14:13 +0700

    Bài tập ứng dụng chọn màu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import {PetComponent} from './pet/pet.component';
 import {FontSizeEditorComponent} from './font-size-editor/font-size-editor.component';
 import {CalculatorComponent} from './calculator/calculator.component';
+import {ColorPickerComponent} from './color-picker/color-picker.component';
 
 const routes: Routes = [
   {path: 'pet', component: PetComponent},
   {path: 'font-size', component: FontSizeEditorComponent},
-  {path: 'calculator', component: CalculatorComponent}
+  {path: 'calculator', component: CalculatorComponent},
+  {path: 'color-picker', component: ColorPickerComponent}
   ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { FontSizeEditorComponent } from './font-size-editor/font-size-editor.com
 import { PetComponent } from './pet/pet.component';
 import {FormsModule} from '@angular/forms';
 import { CalculatorComponent } from './calculator/calculator.component';
+import { ColorPickerComponent } from './color-picker/color-picker.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FontSizeEditorComponent,
     PetComponent,
-    CalculatorComponent
+    CalculatorComponent,
+    ColorPickerComponent
   ],
   imports: [
     BrowserModule,
