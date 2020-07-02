a7168a63a04bd14a449df02517fafd01f2147c8d
huyfr <61008601+huyfr@users.noreply.github.com>
2020-07-02 11:27:49 +0700

    Thêm routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {PetComponent} from './pet/pet.component';
+import {FontSizeEditorComponent} from './font-size-editor/font-size-editor.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'pet', component: PetComponent},
+  {path: 'font-size', component: FontSizeEditorComponent}
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
